9b8a8cef595c233313d90090ac59a8bd291e3225
Benjamin Rianda <reptarforlife@gmail.com>
2015-01-12 00:32:38 -0800

    Map pack first 5 maps
    Cool Sexy Beauty, Honey Suckle, Team Arena, Temple Run, Guma Guma Prime

---
+++ ui/mods/Guma_Guma_Slide_Map_Pack/maplist.js
@@ -0,0 +1,7 @@
+cShareSystems.load_pas("Guma Guma Slide", [
+	"coui://ui/mods/Guma_Guma_Slide_Map_Pack/systems/[ggs]_cool_sexy_beauty.pas",
+	"coui://ui/mods/Guma_Guma_Slide_Map_Pack/systems/[ggs]_guma_guma_prime.pas",
+	"coui://ui/mods/Guma_Guma_Slide_Map_Pack/systems/[ggs]_honey_suckle.pas",
+	"coui://ui/mods/Guma_Guma_Slide_Map_Pack/systems/[ggs]_team_arena.pas",
+	"coui://ui/mods/Guma_Guma_Slide_Map_Pack/systems/[ggs]_temple_run.pas"
+]);
